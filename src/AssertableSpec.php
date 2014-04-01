6b14340fd95328fa99137c5861ea4d2a28b12ed7
Sean Mumford <sean.mumford@gmail.com>
2014-04-01 18:56:40 +0000

    adding basic code docs

--- src/AssertableSpec.php
+++ src/AssertableSpec.php
@@ -4,10 +4,24 @@ namespace Thepsion5\Spec;
 
 use Thepsion5\Spec\Exceptions\BaseSpecUnsatisfiedException;
 
+/**
+ * Class AssertableSpec
+ * @package Thepsion5\Spec
+ */
 abstract class AssertableSpec
 {
+    /**
+     * Checks to see if a specified object meets the specification
+     * @param  mixed $subject The object or value being examined
+     * @return bool           True if the specification is satisfied, false otherwise
+     */
     public abstract function isSatisfiedBy($subject);
 
+    /**
+     * Asserts that a specified object meets the specification
+     * @param mixed $subject The object or value being examined
+     * @throws Exceptions\SpecUnsatisfiedException If the assertion fails
+     */
     public function assertSatisfiedBy($subject)
     {
         if(!$this->isSatisfiedBy($subject)) {
@@ -15,6 +29,12 @@ abstract class AssertableSpec
         }
     }
 
+    /**
+     * Creates and throws an exception after adding references to itself
+     * and the subject.
+     * @param mixed $subject The object or value being examined
+     * @throws Exceptions\BaseSpecUnsatisfiedException
+     */
     protected function assertionFailed($subject)
     {
         $exception = new BaseSpecUnsatisfiedException();
--- src/CompositeAssertableSpec.php
+++ src/CompositeAssertableSpec.php
@@ -3,37 +3,61 @@ namespace Thepsion5\Spec;
 
 use Thepsion5\Spec\Exceptions\SpecUnsatisfiedException;
 
+/**
+ * Class CompositeAssertableSpec
+ * @package Thepsion5\Spec
+ */
 abstract class CompositeAssertableSpec extends AssertableSpec
 {
     /**
-     * Contains and specifications
+     * Contains specifications that must all be satisfied
      * @var array
      */
     protected $andSpecs = [];
 
     /**
-     * Contains or specifications
+     * Contains specifications of which only one must be satisfied
      * @var array
      */
     protected $orSpecs = [];
 
+    /**
+     * Adds a specification to the collection that must be satisfied in order for
+     * the overall spec to be satisfied
+     * @param AssertableSpec $spec Added to the collection of logical and specifications
+     * @return $this
+     */
     public function andSatisfiedBy(AssertableSpec $spec)
     {
         $this->andSpecs[] = $spec;
         return $this;
     }
 
+    /**
+     * Adds a specification to the collection for which only one specification must
+     * be satisfied for the overall spec to be satisfied
+     * @param AssertableSpec $spec Added to the collection of logical or specifications
+     * @return $this
+     */
     public function orSatisfiedBy(AssertableSpec $spec)
     {
         $this->orSpecs[] = $spec;
         return $this;
     }
 
+    /**
+     * @param mixed $subject The object or value being examined
+     * @return bool True if the specification is satisfied based on its components
+     */
     public function isSatisfiedBy($subject)
     {
         return $this->andSpecsSatisfied($subject) && $this->orSpecsSatisfied($subject);
     }
 
+    /**
+     * @param mixed $subject The object or value being examined
+     * @return bool True if the logical and specifications are all satisfied
+     */
     protected function andSpecsSatisfied($subject)
     {
         $satisfied = true;
@@ -46,6 +70,10 @@ abstract class CompositeAssertableSpec extends AssertableSpec
         return $satisfied;
     }
 
+    /**
+     * @param mixed $subject The object or value being examined
+     * @return bool True if at least one logical or specification is satisfied or there are no logical or specifications
+     */
     protected function orSpecsSatisfied($subject)
     {
         if(empty($this->orSpecs)) return true;
@@ -59,12 +87,20 @@ abstract class CompositeAssertableSpec extends AssertableSpec
         return $satisfied;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function assertSatisfiedBy($subject)
     {
         $this->assertAndSpecsSatisfied($subject);
         $this->assertOrSpecsSatisfied($subject);
     }
 
+    /**
+     * Asserts that all logical and specifications are satisfied
+     * @param mixed $subject The object or value being examined
+     * @throws Exceptions\SpecUnsatisfiedException If any of the component specs' assertions fail
+     */
     protected function assertAndSpecsSatisfied($subject)
     {
         foreach($this->andSpecs as $spec) {
@@ -72,6 +108,12 @@ abstract class CompositeAssertableSpec extends AssertableSpec
         }
     }
 
+    /**
+     * Asserts that at least one logical or specification is satisfied, if any exist,
+     * and throws the first failed assertion if none pass
+     * @param mixed $subject The object or value being examined
+     * @throws Exceptions\SpecUnsatisfiedException If none of the logical or spec assertions pass
+     */
     protected function assertOrSpecsSatisfied($subject)
     {
         if(empty($this->orSpecs)) return;
